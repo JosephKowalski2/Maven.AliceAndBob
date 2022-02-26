156c8a414894fa76fc0c10cd1f4fd215051f30fe
Joseph Kowalski <jkjr25@gmail.com>
2022-02-25 20:38:26 -0500

    Completed lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args ){
+    public static void main(String[] args ) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input your first name");
+        String name = scanner.next();
+        if (name.contentEquals("Alice")) {
+            System.out.println("Hello " + name);
+        }
+        if (name.contentEquals("Bob")) {
+            System.out.println("Hello " + name);
+        }
+        scanner.close();
     }
 }
+
